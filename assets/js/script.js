88498b8421d75b8e5e04b18fa65c06b14d0e8726
LevisgtonEustaquio <106932832+LevisgtonEustaquio@users.noreply.github.com>
2023-06-04 18:56:46 +0000

    Apresentar ao grupo e fazer alteração, caso necessario

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,34 +7,3 @@ function topo(){
         }
     )
 }
-//Validação de Login
-
-function login(){
-    var logado = 0;
-    var usuario = document.getElementById("usuario").value;
-    var senha = document.getElementById("senha").value;
-
-    if(usuario == 'admin' && senha == '123'){
-        window.location = "index.html";
-        logado = 1;
-    }
-
-    if(logado == 0){
-        alert("Acesso negado, dados incorretos!");
-    }
-}
-//Ativar alert no botão cadastrar
-
-function cadastro(){
-    alert("Cadastro com sucesso!");
-    window.location.href = "index.html";
-
-    if(usuario == '' && senha == '' && email == ''){
-        window.location = "index.html";
-        logado = 1;
-    }
-
-    if(logado == 0){
-        alert("Acesso negado, dados incorretos!");
-    }
-}
